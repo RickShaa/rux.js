2f95ca882a0e32818d4e13465074d8ce579900a5
RickShaa <r.shaffer@gmx.de>
2022-05-26 17:07:44 +0200

    fixed array clone

--- src/rux.ts
+++ src/rux.ts
@@ -1,34 +1,33 @@
-function cloneState<T> (state:T | T[]){
-    if(typeof state === "object" && 
-    !Array.isArray(state) 
-    && state !==null){
-        return {...state};
-    }
-    else if(Array.isArray(state)) {
-        return Array.from(state);
-    }
-    else{
-        return state;
-    }
+function cloneState<T>(state: T) {
+  if (typeof state === "object" && !Array.isArray(state) && state) {
+    return { ...state };
+  } else if (Array.isArray(state)) {
+    return Object.assign([], state);
+  } else {
+    return state;
+  }
 }
 
-type ReturnState<T> = [() => T | T[], (cb:(state:T) => T) => void];
+type ReturnState<T> = [() => T, (cb: (state: T) => T) => T];
 
-
-function rux <T> (state:T ):ReturnState<T> {
-    let currentState = state;
-    let clone = cloneState(currentState);
-    return [() => clone, (cb:(oldState:T) => T) => {
-        currentState = cb(currentState)
-        clone = cloneState(currentState);
-        return currentState;
-    }];
+function rux<T>(state: T): ReturnState<T> {
+  let currentState = state;
+  let clone = cloneState(currentState);
+  return [
+    () => clone,
+    (cb: (oldState: T) => T) => {
+      currentState = cb(currentState);
+      clone = cloneState(currentState);
+      return currentState;
+    },
+  ];
 }
 
-const [state, setState] = rux({name:"Rick"});
+const [state, setState] = rux({ name: "Rick", age: [1, 2, 3] });
 console.log(state());
-console.log(setState((old) => {
-    return {...old, name: "R"}
-}));
+console.log(
+  setState((old) => {
+    return { ...old, name: "R" };
+  })
+);
 console.log(state());
-
