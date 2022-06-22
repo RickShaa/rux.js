2caeb1ba617c2c1ff44376b2a4b96385aa8c9c26
RickShaa <r.shaffer@gmx.de>
2022-06-22 21:55:31 +0200

    puh..

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
+import { doesNotMatch } from "assert";
 import { initState } from "./rux";
 
-const { setters, getState, setState, $observe } = initState<
-  { todos: { [key: string]: number }[] },
-  "todos"
->({
+interface TodoState {
+  todos: string[];
+  done: string[];
+}
+
+const { setters, getState, setState, $observe } = initState<TodoState>({
   state() {
     return {
       todos: [],
+      done: [],
     };
   },
   setters: {
     addTodos(state, object) {
       state.todos.push(object);
+    },
+    markLastTodoAsDone(state, object) {
+      const lastItem = state.todos.pop();
+      if (lastItem) {
+        state.done.push(lastItem);
+      }
     },
   },
 });
@@ -20,4 +30,6 @@ $observe((state) => {
   console.log(state);
 });
 
-setters.addTodos({ count: 2 });
+setters.addTodos("Hello");
+setters.markLastTodoAsDone();
+setters.markLastTodoAsDone();
--- src/rux.ts
+++ src/rux.ts
@@ -2,37 +2,30 @@ import lodash from "lodash";
 
 type CallBack<T> = (state: T) => void;
 type TObj = { [key: string]: any };
-type SetterObj<T, K extends keyof T> = {
-  [key: string]: (
-    state: T,
-    stateModifier: T[K] extends Array<any> ? T[K][number] : T[K]
-  ) => void;
+type SetterObj<T> = {
+  [key: string]: (state: T, stateModifier?: any) => void;
 };
 
-type InitState<T, K extends keyof T> = {
+// stateModifier: T[K] extends Array<any> ? T[K][number] : T[K]
+type InitState<T> = {
   state: () => T;
-  setters: SetterObj<T, K>;
+  setters: SetterObj<T>;
 };
-export function initState<T extends TObj, K extends keyof T>(
-  init: InitState<T, K>
-) {
+export function initState<T extends TObj>(init: InitState<T>) {
   let observers: CallBack<T>[] = [];
   let currentState = init.state();
   let functionNames: string[] = [];
+
   for (let fn in init.setters) {
     let fnName = fn.toString();
     functionNames.push(fnName);
   }
   let setterState: {
-    [key: typeof functionNames[number]]: (
-      stateModifier: T[K] extends Array<any> ? T[K][number] : T[K]
-    ) => void;
+    [key: typeof functionNames[number]]: (stateModifier?: any) => void;
   };
   setterState = Object.create({});
   functionNames.forEach((functionName) => {
-    setterState[functionName] = (
-      stateModifier: T[K] extends Array<any> ? T[K][number] : T[K]
-    ) => {
+    setterState[functionName] = (stateModifier: any) => {
       init.setters[functionName].apply(null, [currentState, stateModifier]);
       notifyObservers<T>(observers, lodash.cloneDeep(currentState));
     };
